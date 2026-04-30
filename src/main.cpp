a762987d88fac8e0a7ff5ccd040c658ee1fa2e2f
John Stovin <john.d.stovin@gmail.com>
2026-04-30 01:06:23 +0100

    Configuration

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 #include <memory>
 
 #include "sensesp.h"
-#include "sensesp/sensors/sensor.h"
 #include "sensesp/signalk/signalk_output.h"
 #include "sensesp/system/lambda_consumer.h"
 #include "sensesp_app_builder.h"
@@ -54,7 +53,7 @@ void setup() {
                     ->set_hostname("engineroom-monitor")
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
-                    //->set_wifi_client("My WiFi SSID", "my_wifi_password")
+                    ->set_wifi_client("SpringCottage", "!ginger&harry@springcottage!")
                     //->set_wifi_access_point("My AP SSID", "my_ap_password")
                     //->set_sk_server("192.168.10.3", 80)
                     ->get_app();
@@ -129,14 +128,15 @@ void setup() {
   auto temperatureTransform = new LambdaTransform<scd40_data, float>([] (scd40_data data) -> float { return data.temperature + 272.15; });
   auto humidityTransform = new LambdaTransform<scd40_data, float>([] (scd40_data data) -> float { return data.relativeHumidity / 100.0; });
 
-  auto* engine_room_co2 = new RepeatSensor<scd40_data>(sample_interval, co2_callback);
-  const char* co2_path = "environment.inside.engineRoom.co2Concentration";
-  const char* temperature_path = "environment.inside.engineRoom.temperature";
-  const char* humidity_path = "environment.inside.engineRoom.relativeHumidity";
-  engine_room_co2->connect_to(concentrationTransform)->connect_to(new SKOutputInt(co2_path, "PPM"));
-  engine_room_co2->connect_to(temperatureTransform)->connect_to(new SKOutputFloat(temperature_path, "K"));
-  engine_room_co2->connect_to(humidityTransform)->connect_to(new SKOutputFloat(humidity_path, "%"))->connect_to(new LambdaConsumer<float>([&](float val){ mics6814.set_led(0, 31, 0); delay(200); mics6814.set_led(63, 0, 0); return;}));
+  auto engine_room_co2 = new RepeatSensor<scd40_data>(sample_interval, co2_callback);
 
+  auto co2_output = new SKOutputInt("environment.inside.engineRoom.co2Concentration", new SKMetadata("ppm", "Engine room CO2 Concentration", "CO2 concentration in parts per million"));
+  auto temperature_output = new SKOutputFloat("environment.inside.engineRoom.temperature", new SKMetadata("K", "Engine room temperature"));
+  auto humidity_output = new SKOutputFloat("environment.inside.engineRoom.relativeHumidity", new SKMetadata("ratio", "Engine room relative humidity"));
+  
+  engine_room_co2->connect_to(concentrationTransform)->connect_to(co2_output);
+  engine_room_co2->connect_to(temperatureTransform)->connect_to(temperature_output);
+  engine_room_co2->connect_to(humidityTransform)->connect_to(humidity_output)->connect_to(new LambdaConsumer<float>([&](float val){ mics6814.set_led(0, 31, 0); delay(200); mics6814.set_led(63, 0, 0); return;}));
 
   if (!mics6814.init()) {
     ESP_LOGE(__FILE__, "Failed to initialize the gas sensor");
@@ -149,36 +149,41 @@ void setup() {
   auto nh3Transform = new LambdaTransform<MICS6814::Reading, float>([] (MICS6814::Reading data) -> float { return data.nh3; });
   auto oxidisingTransform = new LambdaTransform<MICS6814::Reading, float>([] (MICS6814::Reading data) -> float { return data.oxidising; });
 
-  auto* engine_room_gases = new RepeatSensor<MICS6814::Reading>(sample_interval, [&]()->MICS6814::Reading { return mics6814.read_all(); });
-  const char* reducing_path = "environment.inside.engineRoom.reducingGases";
-  const char* nh3_path = "environment.inside.engineRoom.nh3";
-  const char* oxidising_path = "environment.inside.engineRoom.oxidisingGases";
-  engine_room_gases->connect_to(reducingTransform)->connect_to(new SKOutputFloat(reducing_path, "Ohms"));
-  engine_room_gases->connect_to(nh3Transform)->connect_to(new SKOutputFloat(nh3_path, "Ohms"));
-  engine_room_gases->connect_to(oxidisingTransform)->connect_to(new SKOutputFloat(oxidising_path, "Ohms"));
+  auto engine_room_gases = new RepeatSensor<MICS6814::Reading>(sample_interval, [&]()->MICS6814::Reading { return mics6814.read_all(); });
+  auto reducing_output = new SKOutputFloat("environment.inside.engineRoom.reducingGases", new SKMetadata("Ohms", "Engine room reducing gases", "A resistance value indicating the relative level of reducing gases (CO) in the atmosphere"));
+  auto nh3_output = new SKOutputFloat("environment.inside.engineRoom.nh3", new SKMetadata("Ohms", "Engine room NH3 level", "A resistance value indicating the relative level of Ammonia in the atmosphere"));
+  auto oxidising_output = new SKOutputFloat("environment.inside.engineRoom.oxidisingGases", new SKMetadata("Ohms", "Engine room oxidising gases", "A resistance value indicating the relative level of oxidising gases (NO2) in the atmosphere"));
+  engine_room_gases->connect_to(reducingTransform)->connect_to(reducing_output);
+  engine_room_gases->connect_to(nh3Transform)->connect_to(nh3_output);
+  engine_room_gases->connect_to(oxidisingTransform)->connect_to(oxidising_output);
 
   // Measure exhaust temperature
-  auto* exhaust_temp =
-      new OneWireTemperature(dts, sample_interval, "/exhaustTemperature/oneWire");
-  auto* exhaust_temp_calibration =
-      new Linear(1.0, 0.0, "/exhaustTemperature/linear");
-  auto* exhaust_temp_sk_output = new SKOutputFloat(
-      "propulsion.port.exhaustTemperature", "/exhaustTemperature/skPath", "K");
+  auto exhaust_temp =
+    new OneWireTemperature(dts, sample_interval, "/exhaustTemperature/oneWire");
+  ConfigItem(exhaust_temp)->set_title("Exhaust Temperature Sensor")->set_sort_order(2000);
+  auto exhaust_temp_calibration =
+      new Linear(1.0, 0.0, "/exhaustTemperature/calibration");
+  ConfigItem(exhaust_temp_calibration)->set_title("Exhaust Temperature Calibration")->set_sort_order(1000);
+  auto exhaust_temp_sk_output= new SKOutputFloat(
+      "propulsion.port.exhaustTemperature", new SKMetadata("K", "Exhaust Temperature"));
 
   exhaust_temp->connect_to(exhaust_temp_calibration)
       ->connect_to(exhaust_temp_sk_output);
 
   // Measure alternator temperature
-  auto* alternator_temp =
-      new OneWireTemperature(dts, sample_interval, "/AlternatorTemperature/oneWire");
-  auto* alternator_temp_calibration =
-      new Linear(1.0, 0.0, "/AlternatorTemperature/linear");
-  auto* alternator_temp_sk_output = new SKOutputFloat(
-      "electrical.alternators.0.temperature", "/alternatorTemperature/skPath", "K");
+  auto alternator_temp =
+      new OneWireTemperature(dts, sample_interval, "/alternatorTemperature/oneWire");
+  ConfigItem(alternator_temp)->set_title("Alternator Temperature Sensor")->set_sort_order(2100);
+
+  auto alternator_temp_calibration =
+      new Linear(1.0, 0.0, "/alternatorTemperature/calibration");
+  ConfigItem(alternator_temp_calibration)->set_title("Alternator Temperature Calibration")->set_sort_order(1100);
+  auto alternator_temp_sk_output = new SKOutputFloat(
+      "electrical.alternators.0.temperature", new SKMetadata("K", "Alternator Temperature"));
 
   alternator_temp->connect_to(alternator_temp_calibration)
       ->connect_to(alternator_temp_sk_output);
-  
+
   // To avoid garbage collecting all shared pointers created in setup(),
   // loop from here.
   while (true) {
